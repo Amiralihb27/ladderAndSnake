f80efa9b63bf1ff674df382a1a782eb856841fe1
Amirali <amiralihabibi82@gmail.com>
2023-05-15 22:14:46 +0330

    Edit CreateObject.java

--- src/main/java/ir/ac/kntu/gamelogic/CreateObject.java
+++ src/main/java/ir/ac/kntu/gamelogic/CreateObject.java
@@ -2,33 +2,88 @@ package ir.ac.kntu.gamelogic;
 
 public class CreateObject {
 
-    public void create(){
+    public void create() {
         System.out.println("Enter the size of the board: ");
-        ScannerWrapper sc=new ScannerWrapper();
+        ScannerWrapper sc = new ScannerWrapper();
         int size = ScannerWrapper.getInt();
-        Grid g=new Grid(size);
-        System.out.println("How many ordinary snake do you wana have?");
-        int numOfOrdinary = ScannerWrapper.getInt();
-        AllSnakes allSnakes=new AllSnakes();
-        for(int i=0;i<numOfOrdinary;i++){
-            Snake ordinary=new OrdinarySnake();
-            allSnakes.addSnake(ordinary);
-        }
-        System.out.println("How many kind snake do you wana have?");
-        int numOfKind = ScannerWrapper.getInt();
-        for(int i=0;i<numOfKind;i++){
-            Snake kind=new KindSnake();
-            allSnakes.addSnake(kind);
-        }
-        System.out.println("How many Savage snake do you wana have?");
-        int numOfSavage = ScannerWrapper.getInt();
-        for(int i=0;i<numOfSavage;i++){
-            Snake savage=new SavageSnake();
-            allSnakes.addSnake(savage);
+        Grid g = new Grid(size);
+        AllSnakes allSnakes = new AllSnakes();
+        warning();
+        createOrdinary(allSnakes);
+        createKind(allSnakes);
+        createSavage(allSnakes);
+        information();
+        Player player = new Player();
+        player.setRow(size - 1);
+        g.draw(player, allSnakes);
+    }
+
+
+    public void warning() {
+        System.out.println("lotfan nesbat mar ha be safhe bazi ra raayat konid :) ");
+    }
+
+    public void createOrdinary(AllSnakes allSnakes) {
+        System.out.println("How many ordinary snakes do you wana have?");
+        while (true) {
+            int numOfOrdinary = ScannerWrapper.getInt();
+            if (numOfOrdinary > 0) {
+                for (int i = 0; i < numOfOrdinary; i++) {
+                    Snake ordinary = new OrdinarySnake();
+                    allSnakes.addSnake(ordinary);
+                }
+                break;
+            } else {
+                System.out.println("Number of these snakes should be more than 0.Try again.");
+            }
+
         }
 
-        Player player=new Player();
-        player.setRow(size-1);
-        g.draw(player,allSnakes);
     }
+
+    public void createKind(AllSnakes allSnakes) {
+        System.out.println("How many Kind snakes do you wana have?");
+        while (true) {
+            int numOfKind = ScannerWrapper.getInt();
+            if (numOfKind > 0) {
+                for (int i = 0; i < numOfKind; i++) {
+                    Snake kind = new KindSnake();
+                    allSnakes.addSnake(kind);
+                }
+                break;
+            } else {
+                System.out.println("Number of these snakes should be more than 0.Try again.");
+            }
+
+        }
+    }
+
+    public void createSavage(AllSnakes allSnakes) {
+        System.out.println("How many Savage snakes do you wana have?");
+        while (true) {
+            int numOfSavage = ScannerWrapper.getInt();
+            if (numOfSavage > 0) {
+                for (int i = 0; i < numOfSavage; i++) {
+                    Snake savage = new SavageSnake();
+                    allSnakes.addSnake(savage);
+                }
+                break;
+            } else {
+                System.out.println("Number of these snakes should be more than 0.Try again.");
+            }
+
+        }
+    }
+
+    public void information() {
+        System.out.println("Be aware that:");
+        System.out.println("s and d belongs to ordinary snakes.");
+        System.out.println("L and r belongs to kind snakes. ");
+        System.out.println("$ and D belongs to Savage snakes.");
+        System.out.println("* belongs to player.");
+        System.out.println("***********************************");
+        System.out.println("If the player moves to the tail of one snake , the grid only shows the players character.");
+        System.out.println("*****************************************************************************************");
+    }
+
 }
